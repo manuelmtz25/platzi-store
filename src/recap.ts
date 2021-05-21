fc1a23f2c4bf1dca48930ed2cb75bdef2a47f0a8
Manuel <manuelmtz_25@hotmail.com>
2021-05-21 15:38:45 -0500

    fix problem with prettier and add recap file

---
+++ src/recap.ts
@@ -0,0 +1,25 @@
+const myName = 'Manuel';
+const myAge = 12;
+const suma = (a: number, b: number) => {
+  return a + b;
+};
+suma(1, 2);
+
+class Persona {
+  //   private age;
+  //   private name;
+
+  //   constructor(age: number, name: string) {
+  //       this.age = age;
+  //       this.name = name;
+  //   }
+
+  constructor(private age: number, private name: string) {}
+
+  getSummary() {
+    return `my name is ${this.name}, ${this.age}`;
+  }
+}
+
+const manuel = new Persona(33, 'Manuel');
+manuel.getSummary();
