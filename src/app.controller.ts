92b015175bde1fb3ab95ccaeb5800822cefd0675
Manuel <manuelmtz_25@hotmail.com>
2021-05-31 13:07:08 -0500

    Add controllers with command: generate

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,58 +7,16 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hola mundo!';
   }
 
   @Get('nuevo')
   newEndpoint() {
-    return 'nuevo endpoint MMA';
+    return 'yo soy nuevo';
   }
 
-  // http://localhost:3000/hello
-  @Get('/hello/')
+  @Get('/ruta/')
   hello() {
-    return 'Hello MMA';
-  }
-
-  @Get('products/filter')
-  getProductFilter() {
-    return `Soy un filter`;
-  }
-
-  // @Get('products/:productId')
-  // getProduct(@Param() params: any) {
-  //   return `product ${params.productId}`;
-  // }
-
-  // http://localhost:3000/products/prod1
-  @Get('products/:productId')
-  getProduct(@Param('productId') productId: string) {
-    return `product ${productId}`;
-  }
-
-  // http://localhost:3000/categories/cat1/products/prod1
-  @Get('categories/:id/products/:productId')
-  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
-    return `product ${productId} and ${id}`;
-  }
-
-  // @Get('products')
-  // getProducts(@Query() params: any) {
-  //   const { limit, offset } = params;
-  //   return `products: limit => ${limit} offset => ${offset} `;
-  // }
-
-  // http://localhost:3000/products/?limit=100&offset=200
-  // @Get('products')
-  // getProducts(@Query('limit') limit: number, @Query('offset') offset: number) {
-  //   return `products: limit => ${limit} offset => ${offset} `;
-  // }
-
-  // http://localhost:3000/products/?limit=100
-  // dar un valor por default
-  @Get('products')
-  getProducts(@Query('limit') limit: number, @Query('offset') offset = 44) {
-    return `products: limit => ${limit} offset => ${offset} `;
+    return 'con /sas/';
   }
 }
