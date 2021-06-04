a71df0163b9d4b8b940a6c9ad7cb549e361a7509
Manuel <manuelmtz_25@hotmail.com>
2021-06-03 20:53:53 -0500

    useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,9 @@ import { BrandsController } from './controllers/brands/brands.controller';
 import { ProductsService } from './services/products/products.service';
 import { CategoriesService } from './services/categories/categories.service';
 
+const API_KEY = '123456';
+const API_KEY_PROD = 'PROD_123456';
+
 @Module({
   imports: [],
   controllers: [
@@ -21,6 +24,14 @@ import { CategoriesService } from './services/categories/categories.service';
     CustomersController,
     BrandsController,
   ],
-  providers: [AppService, ProductsService, CategoriesService],
+  providers: [
+    AppService,
+    ProductsService,
+    CategoriesService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
